3762f34b11c8830ca115d7fe2ccb3007670dbca6
Trusttfl <avantway.info@gmail.com>
2025-07-17 14:04:33 +0100

    Update for src directory structure

--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
+const express = require('express');
 require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ 
+  path: path.resolve(__dirname, '.env') 
+});
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI)
